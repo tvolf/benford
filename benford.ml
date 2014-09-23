916605100a2d92657b94a1763ca49c57a2dc2233
tvolf <gtvolf@gmail.com>
2014-09-23 14:39:22 +0300

    New variant of calculations

--- benford.ml
+++ benford.ml
@@ -2,38 +2,33 @@
 
 open Big_int
 
-let rec fib = 
-	let memo = Hashtbl.create 1 in
-	let get h k = try Some (Hashtbl.find h k) with Not_found -> None in
-	fun n ->
-		match n with 
-		| 0 | 1 -> big_int_of_int 1
-		| n -> match get memo n with 
-			 | Some x -> x
-			 | None -> let r = add_big_int (fib (n-1)) (fib (n-2)) in
-					   ( Hashtbl.add memo n r; r)
-;;			
-
-
-let rec calc_freq fib_cnt = 
-    let memo = Array.make 10 0 in 
-   (
-		for i = 0 to (fib_cnt - 1) do 
-			let f = fib i in 
-			let fst = int_of_char (String.get (string_of_big_int f) 0) - int_of_char '0' in 
-			memo.(fst) <- memo.(fst) + 1
+let fib =
+	let a = ref (big_int_of_int 1) and 
+		b = ref (big_int_of_int 1) and 
+		c = ref (big_int_of_int 0) in
+	fun cnt callback ->
+		for i = 0 to (cnt - 1) do 
+			if i < 2 then callback !a
+			else (c := !a; a := add_big_int !a !b; b := !c; callback !a);
 		done;
-		Array.iteri (fun i v -> Printf.printf "%i - %0.3f\n" i 
-					((float_of_int v) *. 100.0 /. (float_of_int fib_cnt))) memo
-	)
 ;;
-			
+
+let calc_freq arr n = 
+	let fst = int_of_char (String.get (string_of_big_int n) 0) - int_of_char '0' in 
+	arr.(fst) <- arr.(fst) + 1
+;;	
+
+let print_freqs arr cnt = 
+	Array.iteri (fun i v -> Printf.printf "%i - %0.3f\n" i 
+				((float_of_int v) *. 100.0 /. (float_of_int cnt))) arr
+;;				
+	
 let () = (
+	let arr = Array.make 10 0 in
 	Printf.printf "Enter max fibonacci number count [1...] : %!";
-	let err_msg = "Enter positive integer number, please..." in
 	let fib_cnt = try Some (int_of_string (input_line stdin)) with Failure "int_of_string" -> None in
 	match fib_cnt with 
-		| Some n when n > 0 ->	calc_freq n
-		| None | Some _ -> print_endline err_msg
+		| Some n when n > 0 ->	(fib n (calc_freq arr); print_freqs arr n)
+		| None | Some _ -> print_endline "Enter positive integer number, please..."
 	)
 ;;
